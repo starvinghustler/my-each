c9d2cda224da72b12ace79a25aaf01fefcbd074c
idobok <idobok12@gmail.com>
2021-11-12 20:09:41 +0200

    done

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each(collection) # put argument(s) here
   # code here
+  x = 0
+  while x < collection.length #while x is smaller than # of indexes
+  yield(collection[x]) #[x] ensures that only one index is yielded at a time
+  x = x + 1
+end
+collection #returns indexes
 end
